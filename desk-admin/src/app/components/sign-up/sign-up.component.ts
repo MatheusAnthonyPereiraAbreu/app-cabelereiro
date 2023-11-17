09a66e501156f445563c611262b39db88977c48a
Matheus Anthony Pereira Abreu <matheus.anthony.pa@outlook.com>
2023-11-17 14:06:51 -0300

    feat: implementação dos componentes restantes

--- desk-admin/src/app/components/sign-up/sign-up.component.ts
+++ desk-admin/src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.services';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
-
+  constructor(
+    public authService: AuthService
+  ) { }
 }
